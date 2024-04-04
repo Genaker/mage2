82644421470963fbe12577a9c2114c562f7e24b2
Yehor Shytikov <yehor.shytikov@cnspartner.com>
2024-04-04 13:42:14 -0400

    add report controller

---
+++ Block/Index/Index.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Mage\Mage\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context   $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
+
---
+++ Controller/Index/Index.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Mage\Mage\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
+
